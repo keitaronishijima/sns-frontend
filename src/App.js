371ad8894ba560cdbaa6e5703d45f1e0ca1155d1
Keitaro Nishijima <keitaro_nishijima@brown.edu>
2022-07-19 21:05:30 +0900

    fixing

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={user ? <Home /> : <Register />} />
-        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </Router>
